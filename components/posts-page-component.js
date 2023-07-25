1509c5027e43c63f3d727f73474d13498c600a50
Dmitry Kralichkin <dm_krl@mail.ru>
2023-07-25 20:27:56 +0400

    first task ready

--- components/posts-page-component.js
+++ components/posts-page-component.js
@@ -5,11 +5,43 @@ import { posts, goToPage } from "../index.js";
 export function renderPostsPageComponent({ appEl }) {
   // TODO: реализовать рендер постов из api
   console.log("Актуальный список постов:", posts);
-
   /**
    * TODO: чтобы отформатировать дату создания поста в виде "19 минут назад"
    * можно использовать https://date-fns.org/v2.29.3/docs/formatDistanceToNow
    */
+  const postHtml = posts.map((post) => {
+    return `<div class="page-container">
+    <div class="header-container"></div>
+    <ul class="posts">
+      <li class="post">
+        <div class="post-header" data-user-id=${post.user.id}>
+            <img src=${post.user.imageUrl} class="post-header__user-image">
+            <p class="post-header__user-name">${post.user.name}</p>
+        </div>
+        <div class="post-image-container">
+          <img class="post-image" src=${post.imageUrl}>
+        </div>
+        <div class="post-likes">
+          <button data-post-id=${post.id} class="like-button" data-isLiked=${post.isLiked}>
+            <img src="./assets/images/like-active.svg" class="like-button-img">
+          </button>
+          <p class="post-likes-text">
+            Нравится: <strong>${post.likes.length}</strong>
+          </p>
+        </div>
+        <p class="post-text">
+          <span class="user-name">${post.user.name}</span>
+          ${post.description}
+        </p>
+        <p class="post-date">
+          ${post.createdAt}
+        </p>
+      </li>
+      <li class="post">
+    </ul>
+  </div>`;
+  })
+
   const appHtml = `
               <div class="page-container">
                 <div class="header-container"></div>
@@ -93,7 +125,7 @@ export function renderPostsPageComponent({ appEl }) {
                 </ul>
               </div>`;
 
-  appEl.innerHTML = appHtml;
+  appEl.innerHTML = postHtml;
 
   renderHeaderComponent({
     element: document.querySelector(".header-container"),
